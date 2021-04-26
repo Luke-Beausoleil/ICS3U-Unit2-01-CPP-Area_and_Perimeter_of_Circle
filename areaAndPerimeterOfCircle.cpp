f5c0bbf8124e93ef585a841907c7ff7a8c10c0ff
Luke Beausoleil <luke.beausoleil@mths.ca>
2021-04-26 15:25:07 +0000

    corrected typos

--- areaAndPerimeterOfCircle.cpp
+++ areaAndPerimeterOfCircle.cpp
@@ -2,19 +2,19 @@
 //
 // Created by: Luke Beausoleil
 // Created on: April 2021
-// This program calculates area and perimter of a circle
+// This program calculates area and perimeter of a circle
 //    with radius 15 mm
 
 #include <iostream>
 #include <cmath>
 
 int main() {
-    std::cout << "If a cicle has radius: " << std::endl;
+    std::cout << "If a circle has radius: " << std::endl;
     std::cout << "15 mm" << std::endl;
     std::cout << "" << std::endl;
     std::cout << "The area is " << M_PI * pow(15, 2) << "mm²" << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "The perimter is " << 2 * M_PI * 15 << "mm" << std::endl;
+    std::cout << "The perimeter is " << 2 * M_PI * 15 << "mm" << std::endl;
     std::cout << "" << std::endl;
     std::cout << "Done." << std::endl;
 }
